92c6748cda5155a10a384b8029af7da058ecc97b
l337quez <l337.ronald@gmail.com>
2020-06-23 19:26:49 -0400

    Create search_pixel_iterate_image.py
    esto es un script que hice para iterar pixel por pixel en una imagen para  encontrar un pixel deseado

---
+++ search_pixel_iterate_image.py
@@ -0,0 +1,46 @@
+#si necesitas encontrar un pixel de un screenshot este script te va funcionar
+
+import os, sys, time
+import pyautogui
+from PIL import Image, ImageDraw
+
+
+#retardo  4 segundos
+time.sleep(4)
+
+ruta= os.getcwd()
+
+# Capturar pantalla en una region delimitada (x,y,height,width).
+screenshot = pyautogui.screenshot(region=(675,270, 155, 90))
+# Guardar imagen.
+screenshot.save(ruta + "\\check load\\screenshot.png")
+#ruta de la imagen
+screenshot_ruta= ruta + "\\check load\\screenshot.png"
+
+#obtenemos el ancho y el alto
+img=Image.open(screenshot_ruta)
+
+# Devuelve una tupla con la anchura y altura de la imagen
+width,height=img.size
+print(height)
+print(width)
+
+#cargamos la imagen
+img = img.load()
+
+i = 0
+j = 0
+bool= False
+while i<height and bool == False:
+    while j<width and bool == False:
+        print(img[j,i])
+        # si encuentra el color RGB sale
+        if (img[j,i] == (51, 51, 90)) == True:
+            print("Ha encontrado el Pixel (0, 71, 158) ")
+            bool=True
+        j = j+1
+    i = i+1
+
+
+print("salio de los ciclos")
+
